cf40fcd27310b3e74701d1b864e68623928d076c
Mahmoud <mahmoudheretani@hotmail.com>
2018-10-02 23:39:10 +0300

    Session #1 2018-10-02

--- ASPCourse/Controllers/HomeController.cs
+++ ASPCourse/Controllers/HomeController.cs
@@ -1,43 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using ASPCourse.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ASPCourse.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public List<Book> Books { get; set; }
+
+
+        public HomeController()
+        {
+            Books = new List<Book>()
+            {
+                new Book()
+                {
+                    Id=1,
+                    Title="The art of sql",
+                    Description="description of sql language in depth"
+                },
+                new Book()
+                {
+                    Id=2,
+                    Title="ASP.Net core 2.1 pro",
+                    Description="aaaaaa"
+                }
+            };
+        }
+
+        public List<Book> GetBooks()
+        {
+            return Books;
+        }
+
+
+        public IActionResult DisplayBooks()
+        {
+            ViewData.Add("A", Books);
+            return View();
+        }
+
+        public IActionResult ViewBook(int bookId)
+        {
+            Book selectedBook = null;
+            foreach (var book in Books)
+            {
+                if (book.Id == bookId)
+                {
+                    selectedBook = book;
+                    break;
+                }
+            }
+            ViewData.Add("Book", selectedBook);
+            return View();
+        }
+
+        public IActionResult GetBooksAsJson()
+        {
+            var rawJson = JsonConvert.SerializeObject(Books);
+            return Content(rawJson);
+        }
+
+        public string Index()
+        {
+            return "Hello world from ASP.NET CORE MVC";
+        }
+        public string GetData()
+        {
+            return "Get data from method";
+        }
+        public IActionResult MyHomePage()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ ASPCourse/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPCourse.Models
+{
+    public class Book
+    {
+        public int Id { set; get; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
